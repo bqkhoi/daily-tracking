88dc7964e0cd44d2807d81ef8a6fc717011aff9b
Khoi Bui <bqkhoi29@gmail.com>
2021-10-24 16:48:23 +0700

    Create daily tracking project

---
+++ src/main/java/bui/family/dailytracking/repositories/ThongTinLuotKhamBenhTrongNgayRepository.java
@@ -0,0 +1,7 @@
+package bui.family.dailytracking.repositories;
+
+import bui.family.dailytracking.domain.ThongTinLuotKhamBenhTrongNgay;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ThongTinLuotKhamBenhTrongNgayRepository extends CrudRepository<ThongTinLuotKhamBenhTrongNgay, Long> {
+}
